1fd1a8f2b610603e0cc91fcea8d750854a22728a
Chris Lewis <chris.lewis@hudl.com>
2020-02-26 15:51:23 +0000

    Initial commit

---
+++ src/PstJamMesh/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PstJamMesh
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new PstJamMeshStack(app, "PstJamMeshStack");
+            app.Synth();
+        }
+    }
+}
---
+++ src/PstJamMesh/PstJamMeshStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace PstJamMesh
+{
+    public class PstJamMeshStack : Stack
+    {
+        internal PstJamMeshStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
